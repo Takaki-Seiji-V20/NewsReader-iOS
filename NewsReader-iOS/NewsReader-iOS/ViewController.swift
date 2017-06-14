1be83944d903cd487ade9d81f424425a646f0ab3
john <vsn2047508@gmail.com>
2017-06-14 10:50:40 +0900

    add table view

--- NewsReader-iOS/NewsReader-iOS/ViewController.swift
+++ NewsReader-iOS/NewsReader-iOS/ViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDataSource ,UITableViewDelegate{
 
     //ニュース一覧データを格納する配列
     var newsDataArray = NSArray()
     
+    @IBOutlet var table :UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Table viewのDataSource参照先指定
+        table.dataSource = self
+        //Table viewのタップ時のdelegate先を指定
+        table.delegate = self
         
         //ニュース情報の取得
         let requestUrl = "http://appcre.net/rss.php"
@@ -32,6 +39,9 @@ class ViewController: UIViewController {
                 //responseDataからキー値"results"を取り出す
                 self.newsDataArray = responseData["results"] as! NSArray
                 print("\(self.newsDataArray)")
+                //ニュース記事を取得したらテーブルビューに表示
+                self.table.reloadData()
+                
             case.failure(let error):
                 print("通信エラー:\(error)")
             }
@@ -41,6 +51,52 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //セルに表示する内容を設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //storyboardで取得したcellを取得
+        let cell = UITableViewCell(style:UITableViewCellStyle.subtitle,reuseIdentifier:"Cell")
+        //ニュース記事データを取得(配列の"indexPath.row"番目の要素を取得)
+        let newsDic = newsDataArray[indexPath.row] as! NSDictionary
+        cell.textLabel!.text = newsDic["title"] as? String
+        cell.textLabel!.numberOfLines = 3
+        cell.detailTextLabel!.text = newsDic["publishedDate"] as? String
+        
+        return cell
+    }
+    
+    //テーブルビューのせるの数をnewsDataArrayに格納しているデータの数で設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // セルの数を設定
+        return newsDataArray.count
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //セルのインデックスパス番号の出力
+        print("タップされたセルのインデックスパス :\(indexPath.row)")
+        //ニュース記事のデータを取得(配列の要素で"indexPath.row"番目の要素を取得)
+        let newsDic = newsDataArray[indexPath.row] as! NSDictionary
+        //ニュース記事のURLを取得
+        let newsUrl = newsDic["unescapedUrl"] as! String
+        //StringをURLに変換
+        let url = URL(string:newsUrl)
+        //UIApplocation インスタンス作成
+        UIApplication.shared.openURL(url!)
+        //openURLメソッドでURLを引数にWEBブラウザSafariを起動
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // セルの高さを設定
+        return 64
+    }
+    
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        // アクセサリボタン（セルの右にあるボタン）がタップされた時の処理
+        print("タップされたアクセサリがあるセルのindex番号: \(indexPath.row)")
     }
 }
 
